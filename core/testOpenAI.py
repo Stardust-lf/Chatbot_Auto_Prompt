06fb4b4e88bd6dc002405348130a5844a26348e8
Stardust-lf <lifan20010110@163.com>
2024-03-27 20:00:45 +0800

    Base

---
+++ core/testOpenAI.py
@@ -0,0 +1,7 @@
+import os
+from langchain_openai import OpenAI
+
+
+llm = OpenAI(openai_api_key = "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", temperature=0.9)
+xxlimited = sdds(llm)
+print(llm.invoke('Hi'))
